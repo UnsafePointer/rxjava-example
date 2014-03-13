fbf112b90c258bdab22ab93fc694c6d9dd610120
Renzo Crisostomo <ruenzuo@gmail.com>
2014-03-13 13:37:09 +0100

    Pull to refresh added

--- WeatherApp/src/main/java/com/ruenzuo/weatherapp/activities/CitiesActivity.java
+++ WeatherApp/src/main/java/com/ruenzuo/weatherapp/activities/CitiesActivity.java
@@ -3,6 +3,7 @@ package com.ruenzuo.weatherapp.activities;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.ruenzuo.weatherapp.R;
 import com.ruenzuo.weatherapp.managers.WeatherAPIManager;
@@ -15,15 +16,25 @@ import rx.Subscription;
 import rx.android.observables.AndroidObservable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
+import uk.co.senab.actionbarpulltorefresh.extras.actionbarcompat.PullToRefreshLayout;
+import uk.co.senab.actionbarpulltorefresh.library.ActionBarPullToRefresh;
+import uk.co.senab.actionbarpulltorefresh.library.listeners.OnRefreshListener;
 
-public class CitiesActivity extends ActionBarActivity implements Observer<ArrayList<City>> {
+public class CitiesActivity extends ActionBarActivity implements Observer<ArrayList<City>>, OnRefreshListener {
 
     private Subscription subscription;
+    private PullToRefreshLayout ptrLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ptrLayout = (PullToRefreshLayout) findViewById(R.id.ptrLayout);
+        ActionBarPullToRefresh.from(this)
+                .allChildrenArePullable()
+                .listener(this)
+                .setup(ptrLayout);
+        ptrLayout.setRefreshing(true);
         subscription = AndroidObservable.fromActivity(this, WeatherAPIManager.INSTANCE.getCities())
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
@@ -37,16 +48,22 @@ public class CitiesActivity extends ActionBarActivity implements Observer<ArrayL
 
     @Override
     public void onCompleted() {
-
+        ptrLayout.setRefreshing(false);
     }
 
     @Override
     public void onError(Throwable e) {
-
+        ptrLayout.setRefreshing(false);
     }
 
     @Override
     public void onNext(ArrayList<City> cities) {
-        Log.d("CitiesActivity", cities.toString());
+
     }
+
+    @Override
+    public void onRefreshStarted(View view) {
+
+    }
+
 }
