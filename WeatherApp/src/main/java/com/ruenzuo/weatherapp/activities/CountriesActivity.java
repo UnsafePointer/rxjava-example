9e0ff9812d7ba1556dca654e492e7c7c57da3912
Renzo Crisostomo <ruenzuo@gmail.com>
2014-03-18 15:42:25 +0100

    Fixing pull to refresh with Fragment

--- WeatherApp/src/main/java/com/ruenzuo/weatherapp/activities/CountriesActivity.java
+++ WeatherApp/src/main/java/com/ruenzuo/weatherapp/activities/CountriesActivity.java
@@ -2,50 +2,24 @@ package com.ruenzuo.weatherapp.activities;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.View;
 
 import com.ruenzuo.weatherapp.R;
 import com.ruenzuo.weatherapp.fragments.CountriesListFragment;
 import com.ruenzuo.weatherapp.models.Country;
 
-import uk.co.senab.actionbarpulltorefresh.extras.actionbarcompat.PullToRefreshLayout;
-import uk.co.senab.actionbarpulltorefresh.library.ActionBarPullToRefresh;
-import uk.co.senab.actionbarpulltorefresh.library.listeners.OnRefreshListener;
-
-public class CountriesActivity extends ActionBarActivity implements OnRefreshListener,
-        CountriesListFragment.OnCitySelectedListener, CountriesListFragment.OnLoadCitiesListener {
-
-
-    private PullToRefreshLayout ptrLayout;
+public class CountriesActivity extends ActionBarActivity implements
+        CountriesListFragment.OnCountrySelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cities);
+        setContentView(R.layout.activity_country);
         setTitle("Countries");
-        ptrLayout = (PullToRefreshLayout) findViewById(R.id.ptrLayout);
-        ActionBarPullToRefresh.from(this)
-                .listener(this)
-                .setup(ptrLayout);
     }
 
     @Override
-    public void onRefreshStarted(View view) {
+    public void onCountrySelected(Country country) {
 
     }
 
-    @Override
-    public void onCitySelected(Country country) {
-
-    }
-
-    @Override
-    public void onBeginLoadCities() {
-        ptrLayout.setRefreshing(true);
-    }
-
-    @Override
-    public void onFinishLoadCities() {
-        ptrLayout.setRefreshing(false);
-    }
 }
--- WeatherApp/src/main/java/com/ruenzuo/weatherapp/fragments/CountriesListFragment.java
+++ WeatherApp/src/main/java/com/ruenzuo/weatherapp/fragments/CountriesListFragment.java
@@ -3,6 +3,9 @@ package com.ruenzuo.weatherapp.fragments;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.ruenzuo.weatherapp.R;
 import com.ruenzuo.weatherapp.adapters.CountriesAdapter;
@@ -11,55 +14,62 @@ import com.ruenzuo.weatherapp.models.Country;
 
 import java.util.ArrayList;
 
+import rx.Observable;
 import rx.Observer;
 import rx.Subscription;
 import rx.android.observables.AndroidObservable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
+import uk.co.senab.actionbarpulltorefresh.extras.actionbarcompat.PullToRefreshLayout;
+import uk.co.senab.actionbarpulltorefresh.library.ActionBarPullToRefresh;
+import uk.co.senab.actionbarpulltorefresh.library.listeners.OnRefreshListener;
 
-public class CountriesListFragment extends ListFragment implements Observer<ArrayList<Country>> {
+public class CountriesListFragment extends ListFragment implements Observer<ArrayList<Country>>, OnRefreshListener {
 
     private Subscription subscription;
+    private Observable observable;
 
-    public interface OnCitySelectedListener {
-        public void onCitySelected(Country country);
+    public interface OnCountrySelectedListener {
+        public void onCountrySelected(Country country);
     }
 
-    public interface OnLoadCitiesListener {
-        public void onBeginLoadCities();
-        public void onFinishLoadCities();
-    }
+    private OnCountrySelectedListener countrySelectedListener;
 
-    private OnCitySelectedListener citySelectedListener;
-    private OnLoadCitiesListener loadCitiesListener;
+    private PullToRefreshLayout pullToRefreshLayout;
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
-        if (activity instanceof OnCitySelectedListener) {
-            citySelectedListener = (OnCitySelectedListener)activity;
+        if (activity instanceof OnCountrySelectedListener) {
+            countrySelectedListener = (OnCountrySelectedListener)activity;
         } else {
             throw new ClassCastException(activity.toString()
-                    + " must implement MyListFragment.OnCitySelectedListener");
-        }
-        if (activity instanceof OnLoadCitiesListener) {
-            loadCitiesListener = (OnLoadCitiesListener)activity;
-        } else {
-            throw new ClassCastException(activity.toString()
-                    + " must implement MyListFragment.OnLoadCitiesListener");
+                    + " must implement MyListFragment.OnCountrySelectedListener");
         }
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        loadCitiesListener.onBeginLoadCities();
         CountriesAdapter adapter = new CountriesAdapter(getActivity(), R.layout.row_city);
         setListAdapter(adapter);
-        subscription = AndroidObservable.fromFragment(this, WeatherAPIManager.INSTANCE.getCities())
+        pullToRefreshLayout.setRefreshing(true);
+        observable = WeatherAPIManager.INSTANCE.getCities();
+        subscription = AndroidObservable.fromFragment(this, observable)
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_countries, container, false);
+        pullToRefreshLayout = (PullToRefreshLayout) view.findViewById(R.id.ptrLayout);
+        ActionBarPullToRefresh.from(getActivity())
+                .allChildrenArePullable()
+                .listener(this)
+                .setup(pullToRefreshLayout);
+        return view;
     }
 
     @Override
@@ -69,13 +79,18 @@ public class CountriesListFragment extends ListFragment implements Observer<Arra
     }
 
     @Override
+    public void onRefreshStarted(View view) {
+
+    }
+
+    @Override
     public void onCompleted() {
-        loadCitiesListener.onFinishLoadCities();
+        pullToRefreshLayout.setRefreshing(false);
     }
 
     @Override
     public void onError(Throwable e) {
-        loadCitiesListener.onFinishLoadCities();
+        pullToRefreshLayout.setRefreshing(false);
     }
 
     @Override
